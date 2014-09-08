4f4b624d40538c90333214c1f57a7c84666a8f38
trungptdhcn <trungptdhcn@gmail.com>
2014-09-08 21:36:47 +0700

    update

---
+++ src/com/example/SexPartner/ui/TimeToMatchActivity.java
@@ -0,0 +1,17 @@
+package com.example.SexPartner.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.example.SexPartner.R;
+
+/**
+ * Created by Jodie Pham on 9/6/14.
+ */
+public class TimeToMatchActivity extends Activity
+{
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.time_to_match);
+    }
+}
